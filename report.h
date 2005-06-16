a984b0224ebcd27c33b72a5a504addb29071d58f
Raphaël Assénat <raph@raphnet.net>
2005-06-16 16:48:55 +0000

    5 bytes wide

--- report.h
+++ report.h
@@ -13,7 +13,7 @@ extern int last_pc;
 
 #define report_memread(addr) do { int idx = (((addr)&0xff00)<<4)+2; idx+= ((addr)%256)<<3; memsurface_data[idx]=0xff; memsurface_data[idx+2048]=0xff; memsurface_data[idx+4]=0xff; memsurface_data[idx+4+2048]=0xff; used2[((addr)&0xff00)>>8]=1; } while(0)
 
-#define report_memread2(addr, opcode) do { int i, idx; if ((addr)==last_pc) { break; } else { for (i=0; i<5; i++) { idx = (((addr)+i)&0xff00)<<4; idx+= ((addr)%256)<<3; memsurface_data[idx]=0xff; memsurface_data[idx+2048]=0xff; memsurface_data[idx+4]=0xff; memsurface_data[idx+4+2048]=0xff; used2[((addr)&0xff00)>>8]=1;  } } } while(0)
+#define report_memread2(addr, opcode) do { int i, idx; if ((addr)==last_pc) { break; } else { for (i=0; i<5; i++) { idx = (((addr)+i)&0xff00)<<4; idx+= (((addr)+i)%256)<<3; memsurface_data[idx]=0xff; memsurface_data[idx+2048]=0xff; memsurface_data[idx+4]=0xff; memsurface_data[idx+4+2048]=0xff; used2[((addr)&0xff00)>>8]=1;  } } } while(0)
 
 #define report_memwrite(addr) do { int idx = (((addr)&0xff00)<<4)+1; idx += ((addr) % 256)<<3; memsurface_data[idx]=0xff; memsurface_data[idx+4] = 0xff; memsurface_data[idx+2048]=0xff; memsurface_data[idx+2048+4] = 0xff; } while(0)
 
