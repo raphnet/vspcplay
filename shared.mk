41c9fe5ad5893621c733f37d0563c6668a4bf72a
Project_HSI <57279339+ProjectHSI@users.noreply.github.com>
2023-12-29 14:48:30 +1000

    Fix dynamic OBJs

--- shared.mk
+++ shared.mk
@@ -1,3 +1,8 @@
 VERSION_STR := 1.5
 
-OBJS := $(subst .c,.o,$(subst .cpp,.o,$(wildcard **.c*)))
+OBJS := $(wildcard **.c*)               # Initial pass of OBJs
+OBJS := $(subst .cpp,.o,$(OBJS))        # Filter C++ files.
+                                       # This needs to be done first otherwise the next step will corrupt the OBJ list.
+OBJS := $(subst .c,.o,$(OBJS))          # Filter C files.
+
+OBJS := $(filter-out getopt1.o,$(OBJS)) # Compiling this specific file causes the build to fail.
