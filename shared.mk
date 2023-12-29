75370e7745e1c035256612eb7d5b90bb045054ea
Project_HSI <57279339+ProjectHSI@users.noreply.github.com>
2023-12-29 15:45:16 +1000

    update dynamic objects

--- shared.mk
+++ shared.mk
@@ -1,3 +1,7 @@
 VERSION_STR := 1.5
 
-OBJS := $(subst .c,.o,$(subst .cpp,.o,$(wildcard *.c*)))
+OBJS := $(wildcard *.c*)
+OBJS := $(subst .cpp,.o,$(OBJS))
+OBJS := $(subst .c,.o,$(OBJS))
+
+OBJS := $(filter-out getopt1.o,$(OBJS))
